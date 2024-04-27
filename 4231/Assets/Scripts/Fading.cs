e3f3172d71e5e10587745d4fc850b73457a8f4a4
Kyle202034 <kyleweston2002@gmail.com>
2024-04-26 21:12:57 -0400

    imported the logo and set up some of the fading code
    The fading code is marked off as I'm still working on the implementation

---
+++ 4231/Assets/Scripts/Fading.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fading : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup myUIGroup;
+
+    [SerializeField] private bool fadeIn = false;
+    [SerializeField] private bool fadeOut = false;
+    
+    public void ShowUI()
+    {
+        fadeIn = true;
+    }
+
+    public void HideUI()
+    {
+        fadeOut = true;
+    }
+    private void Start()
+    {
+        //StartCoroutine(CoroutineFade());
+    }
+    private void Update()
+    {
+
+        if (fadeIn)
+        {
+            if (myUIGroup.alpha < 1)
+            {
+                myUIGroup.alpha += Time.deltaTime;
+                if (myUIGroup.alpha >= 1)
+                {
+                    fadeIn = false;
+                }
+            }
+        }
+        if (fadeOut)
+        {
+            if (myUIGroup.alpha >= 0)
+            {
+                myUIGroup.alpha -= Time.deltaTime;
+                if (myUIGroup.alpha == 0)
+                {
+                    fadeOut = false;
+                }
+            }
+        }
+    }
+    
+    IEnumerator CoroutineFade()
+    {
+        yield return new WaitForSeconds(1);
+
+        ShowUI();
+    }
+}
